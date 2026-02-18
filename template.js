4680c58177a0cd7dbea3df25d47475944f274edf
SalawinTheDucktective <peksalawins@gmail.com>
2026-02-18 20:13:41 +0700

    ฟหก

--- template.js
+++ template.js
@@ -4,3 +4,18 @@ const navMenu = document.getElementById('nav-menu');
 mobileMenu.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+function toggleDropdown(e) {
+    // เช็คหน้าจอ ถ้าเล็กกว่า 768px (มือถือ) ให้ทำงาน
+    if (window.innerWidth <= 768) {
+        e.preventDefault(); // กันไม่ให้ลิงก์เด้ง
+        const dropdown = document.getElementById("joinDropdown");
+        
+        // สลับคลาส show (เปิด/ปิด)
+        if (dropdown.style.display === "block") {
+            dropdown.style.display = "none";
+        } else {
+            dropdown.style.display = "block";
+        }
+    }
+}
